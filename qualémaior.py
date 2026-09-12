bdfff7e75ac113fb7839efab8d6dd8a6fa7cd974
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-09-12 15:09:44 -0300

    Add files via upload

---
+++ qualémaior.py
@@ -0,0 +1,9 @@
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+if valor1 > valor2:
+    maior=valor1
+else:
+    maior=valor2
+
+print(f"o maior é {maior}")
