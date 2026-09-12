beb41495b51b6fb198826348cccc57d938a4f7ff
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-09-12 15:52:42 -0300

    Add files via upload

---
+++ divisivelpor2e3.py
@@ -0,0 +1,7 @@
+A = int(input("Digite um número inteiro: "))
+
+if A % 2 == 0 and A % 3 == 0:
+    print("É divisível por 2 e por 3 ao mesmo tempo.")
+else:
+    print("Não é divisível por 2 e por 3 ao mesmo tempo.")
+
