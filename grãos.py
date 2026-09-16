f05825ae4d43ed8161aa41e0a35f34ba79584bb7
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-09-16 20:42:10 -0300

    Add files via upload

---
+++ grãos.py
@@ -0,0 +1,22 @@
+casa = 1
+graos = 1
+total = 0
+
+while casa <= 64:
+    total = total + graos
+    graos = graos * 2
+    casa = casa + 1
+
+print(f"O total de grãos no tabuleiro é: {total}")
+
+
+
+
+
+
+
+
+
+
+
+
