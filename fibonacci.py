011f85dbb10a2e15b488a72fd8124af00a7ebf7d
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-09-16 19:53:36 -0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+N = int(input("Digite a quantidade de termos: "))
+n: int = 1
+a: int = 0
+b: int = 1
+
+while n <= N:
+    print(a)
+
+    proximo = a + b
+    a = b
+    b = proximo
+
+    n += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
